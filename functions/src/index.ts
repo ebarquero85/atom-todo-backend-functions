e6dbd9d4b0cbae05e275c06031b5b9caee50661d
Edgard Barquero <ebarquero85@gmail.com>
2024-12-15 11:47:35 -0600

    Se cargan datos del primer endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,37 @@
-import { https } from "firebase-functions";
-import { initializeApp } from "firebase-admin/app";
-import express, { Request, Response } from "express";
+import { https } from 'firebase-functions';
+import { initializeApp, applicationDefault } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
 
-// Inicializar Firebase Admin
-initializeApp();
+dotenv.config({ path: './.env' });
 
-// Crear una instancia de Express
+initializeApp({
+  credential: applicationDefault(),
+});
+
+const db = getFirestore();
+
 const app = express();
 
-app.get("/hello", (req: Request, res: Response) => {
-  res.status(200).send("<h1>Hello World desde Firebase Functions!</h1>");
+// console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
+app.get('/hello', async (req: Request, res: Response) => {
+  try {
+    const querySnapshot = await db.collection('users').get();
+
+    if (!querySnapshot.empty) {
+      const docData = querySnapshot.docs[0].data();
+      const email = docData.email;
+
+      res.status(200).send(`<h1>Hello World ${email}</h1>`);
+    } else {
+      res.status(200).send('<h1>No hay datos</h1>');
+    }
+  } catch (error) {
+    console.error('Error al obtener documentos:', error);
+    res.status(400).send('<h1>OCURRIO UN ERROR AL OBTENER LA DATA</h1>');
+  }
 });
 
 exports.api = https.onRequest(app);
